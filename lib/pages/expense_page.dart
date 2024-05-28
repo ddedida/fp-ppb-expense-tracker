7aaf682ddcb54305941a4d04eee9646076424ea3
Fiqi <fiqi296@gmail.com>
2024-05-28 15:52:34 +0700

    fix: expense list

--- lib/pages/expense_page.dart
+++ lib/pages/expense_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'ppb_expense_tracker/model/expenses.dart';
+import '../model/expenses.dart';
+import '../infrastructure/db/expenses.dart';
 
 class ExpensePage extends StatefulWidget {
   const ExpensePage({super.key});
@@ -9,11 +10,39 @@ class ExpensePage extends StatefulWidget {
 }
 
 class _ExpensePageState extends State<ExpensePage> {
-  List<Expense> expenses;
+  late List<Expense> expenses;
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    refreshExpenses();
+  }
+
+  @override
+  void dispose() {
+    ExpensesDatabases.instance.close();
+    super.dispose();
+  }
+
+  Future refreshExpenses() async {
+    setState(() => isLoading = true);
+
+    expenses = await ExpensesDatabases.instance.readAllExpenses();
+
+    setState(() => isLoading = false);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(),
+      body: Center(
+        child: isLoading
+            ? const CircularProgressIndicator()
+            : expenses.isEmpty
+                ? const Text('No expenses')
+                : buildCard(),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.of(context).pushNamed('/expense/add');
@@ -22,4 +51,21 @@ class _ExpensePageState extends State<ExpensePage> {
       ),
     );
   }
+
+  Widget buildCard() => ListView.builder(
+        itemCount: expenses.length,
+        itemBuilder: (context, index) {
+          final expense = expenses[index];
+
+          return Card(
+            margin: const EdgeInsets.all(8.0),
+            elevation: 4.0,
+            child: ListTile(
+              title: Text(expense.title ?? 'No title'),
+              subtitle: Text(expense.amount.toString()),
+              trailing: Text(expense.date),
+            ),
+          );
+        },
+      );
 }
